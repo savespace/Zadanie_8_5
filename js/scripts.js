d4ba2c9fc521340f1fd95177ef8272c8b498d86d
savespace <savespace@outlook.com>
2017-07-25 23:52:45 +0200

    First update

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// scripts.js //
+alert('Wciśnij F12 aby zobaczyć pole trójkąta');
+//Pole trójkąta//
+var a = 5;
+var h = 10;
+var triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
